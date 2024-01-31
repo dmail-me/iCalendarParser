e11ef05196d69dea268ccfb04c4839ef664d5bc4
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2024-01-31 21:25:32 +0100

    test for correct hour cycle

--- Tests/iCalendarParserTests/DateTimeTypeTests.swift
+++ Tests/iCalendarParserTests/DateTimeTypeTests.swift
@@ -26,4 +26,18 @@ final class DateTimeTypeTests: XCTestCase {
         let wrongDate = dateTime.type.dateFormatter().date(from: "19700327T020000")
         XCTAssertNotEqual(dateTime.date, wrongDate)
     }
+    
+    func testCorrectHourCycle() {
+        // to test this toggle off/on 24-hour time in settings
+        let property = ICProperty("DTSTART", "19700329T020000")
+        let dateTime = PropertyBuilder.buildDateTime(from: property)!
+        let dateFormatter = dateTime.type.dateFormatter()
+        let currentLocale = Locale.current
+        if (currentLocale.is12HoursFormat()){
+            XCTAssert(dateFormatter.locale.hourCycle == .oneToTwelve)
+        }
+        else {
+            XCTAssert(dateFormatter.locale.hourCycle == .zeroToTwentyThree)
+        }
+    }
 }
